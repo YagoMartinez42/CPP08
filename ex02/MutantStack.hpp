064280b8a47c7e56eb8cf2f9128a01d471c03010
samartin <samartin@student.42madrid.com>
2025-11-12 15:08:45 +0100

    ex02 done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -4,11 +4,11 @@
 # include <stack>
 
 template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T>
+class MutantStack : public std::stack<T, Container>
 {
 public:
 	MutantStack() {}
-	MutantStack(const MutantStack& orig) : std::stack<T>(orig) {}
+	MutantStack(const MutantStack& orig) : std::stack<T, Container>(orig) {}
 	~MutantStack() {}
 
 	MutantStack& operator=(const MutantStack& orig)
@@ -31,33 +31,33 @@ public:
 	}
 
 	typedef typename Container::const_iterator const_iterator;
-	const_iterator cbegin() const
+	const_iterator begin() const
 	{
-		return (this->c.cbegin());
+		return (this->c.begin());
 	}
-	const_iterator cend() const
+	const_iterator end() const
 	{
-		return (this->c.cend());
+		return (this->c.end());
 	}
 
 	typedef typename Container::reverse_iterator reverse_iterator;
-	iterator rbegin()
+	reverse_iterator rbegin()
 	{
 		return (this->c.rbegin());
 	}
-	iterator rend()
+	reverse_iterator rend()
 	{
 		return (this->c.rend());
 	}
 
 	typedef typename Container::const_reverse_iterator const_reverse_iterator;
-	iterator crbegin() const
+	const_reverse_iterator rbegin() const
 	{
-		return (this->c.crbegin());
+		return (this->c.rbegin());
 	}
-	iterator crend() const
+	const_reverse_iterator rend() const
 	{
-		return (this->c.crend());
+		return (this->c.rend());
 	}
 };
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,14 @@
 /*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 13:54:58 by samartin          #+#    #+#             */
-/*   Updated: 2025/09/12 14:24:43 by samartin         ###   ########.fr       */
+/*   Updated: 2025/11/12 14:54:24 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 #include "MutantStack.hpp"
+#include <list>
+#include <deque>
 
 int main(void) {
 	std::cout << std::endl;
@@ -42,11 +44,70 @@ int main(void) {
 		std::stack<int> s(mstack);
 	}
 	std::cout << std::endl;
-	std::cout << TSTH << "===========================================================" << TSTR;
-	std::cout << TSTH << "|  More tests  |" << TSTR;
-	std::cout << TSTH << "===========================================================" << TSTR;
+	std::cout << TSTH << "==========================" << TSTR;
+	std::cout << TSTH << "|  Comparison with list  |" << TSTR;
+	std::cout << TSTH << "==========================" << TSTR;
 	{
+		std::list<int> lst;
+		std::list<int>::iterator li;
+		std::list<int>::reverse_iterator rli;
+		MutantStack<int> mstack;
+		MutantStack<int>::iterator mi;
+		MutantStack<int>::reverse_iterator rmi;
+		mstack.push(5);
+		mstack.push(-17);
+		mstack.push(-3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		mstack.push(1000);
+		lst.push_back(5);
+		lst.push_back(-17);
+		lst.push_back(-3);
+		lst.push_back(5);
+		lst.push_back(737);
+		lst.push_back(0);
+		lst.push_back(1000);
+		std::cout << TSTL << "Printing mutant stack from begin to end" << TSTR;
+		for (mi = mstack.begin(); mi != mstack.end(); mi++)
+			std::cout << *mi << "\n";
+		std::cout << std::endl;
+		std::cout << TSTL << "Printing list from begin to end" << TSTR;
+		for (li = lst.begin(); li != lst.end(); li++)
+			std::cout << *li << "\n";
+		std::cout << std::endl;
+		std::cout << TSTL << "Printing mutant stack reversely" << TSTR;
+		for (rmi = mstack.rbegin(); rmi != mstack.rend(); rmi++)
+			std::cout << *rmi << "\n";
+		std::cout << std::endl;
+		std::cout << TSTL << "Printing list reversely" << TSTR;
+		for (rli = lst.rbegin(); rli != lst.rend(); rli++)
+			std::cout << *rli << "\n";
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << TSTH << "=====================" << TSTR;
+	std::cout << TSTH << "|  Const iterators  |" << TSTR;
+	std::cout << TSTH << "=====================" << TSTR;
+	{
+		MutantStack<bool>::const_iterator mi;
+		MutantStack<bool>::const_reverse_iterator rmi;
+		MutantStack<bool> mstack;
 
+		mstack.push(true);
+		mstack.push(false);
+		mstack.push(true);
+		mstack.push(true);
+		mstack.push(false);
+
+		std::cout << TSTL << "Printing mutant stack from begin to end" << TSTR;
+		for (mi = mstack.begin(); mi != mstack.end(); mi++)
+			std::cout << *mi << "\n";
+		std::cout << std::endl;
+		std::cout << TSTL << "Printing mutant stack reversely" << TSTR;
+		for (rmi = mstack.rbegin(); rmi != mstack.rend(); rmi++)
+			std::cout << *rmi << "\n";
+		std::cout << std::endl;
 	}
 	return (0);
 }
